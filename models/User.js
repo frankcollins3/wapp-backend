5af386f710c69f923a343f5b3b5b2bc647c6e481
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-12-18 18:52:00 -0500

    [index] Add framework for User model

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// User Schema
+const userSchema = new Schema({
+    
+});
+
+module.exports = User = mongoose.model('User', userSchema);;
